e83d4f396f7b2958bd1f2615d1160f0c985765f4
Bryan Sills <bryan@bryansills.com>
2019-02-17 16:15:12 -0600

    using provider for text

--- app/src/main/java/ninja/bryansills/testapp/MainFragment.kt
+++ app/src/main/java/ninja/bryansills/testapp/MainFragment.kt
@@ -4,10 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 
 class MainFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_main, container, true)
+        val view = inflater.inflate(R.layout.fragment_main, container, true)
+        val provider = view.context.applicationContext as Provider
+        val textView = view.findViewById<TextView>(R.id.textView)
+        textView.text = provider.getThing()
+        return view
     }
 }
